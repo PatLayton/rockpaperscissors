d768fb2fc057aaf62f1749a6be5199bc3892b749
PatLayton <patrick.layton@proton.me~>
2024-07-14 18:22:53 -0500

    human and computer choices + prompt

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    // Random # between 0 & 3
+
+    switch(randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2: 
+            return "scissors";    
+    }
+}
+
+function getHumanChoice() {
+    // Prompt for their choice
+    let choice = prompt("Please choose either 'rock', 'paper', or 'scissors':").toLowerCase();
+
+    // Validate the input
+    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+        choice = prompt("Invalid choice! Please choose either 'rock', 'paper', or 'scissors':").toLowerCase();
+    }
+
+    // Return the choice
+    return choice;
+}
+
+
+console.log(getHumanChoice());
